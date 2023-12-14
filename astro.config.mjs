188b6171727ecbbf352bd3300b9e23d6b496455d
gaintopskills <raynafit@yahoo.com>
2023-12-13 17:03:59 -0800

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
+ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://lgdryer.repair//',
